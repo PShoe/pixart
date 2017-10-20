bd48866ef50f6cfbb2e0fbae924cd737c34269e5
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2017-10-20 15:43:58 +1100

    still working on the last one

--- pixart.js
+++ pixart.js
@@ -1,11 +1,13 @@
 var $color_button = $('#set-color');
-var $input = $('#color-field');
+var $color_input = $('#color-field');
+var $movie_input = $('#movie-name');
+var $movie_button = $('#set-movie');
 var $brush_box = $('.brush');
 
 $color_button.on('click', function(event) {
   event.preventDefault();
   $brush_box.css({
-    'background-color': $input.val()
+    'background-color': $color_input.val()
   })
 });
 
@@ -16,6 +18,29 @@ for (i = 0; i < 1000 ; i++) {
 
 var $squares_all = $('.square')
 $squares_all.on('mouseover', function(event){
-  $(event.target).css({'background-color': $input.val()
+  $(event.target).css({'background-color': $color_input.val()
 });
 })
+
+// DID NOT FINISH THE BELOW
+
+$color_button.on('click', function(event){
+  debugger
+  event.preventDefault();
+// EXAMPLE: http://omdbapi.com/?t=Jaws&apikey=2f6435d9
+  var options = {
+    url: 'http://omdbapi.com/',
+    method: 'post',
+    data: {
+      t: $movie_input.val(),
+      apikey: '2f6435d9'
+    }
+  }
+  var appendComment = function(response) {
+    console.log(response);
+    // img- response['Poster']
+  }
+
+  $.ajax(options).done(appendComment);
+
+});
